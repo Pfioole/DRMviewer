c5f26d1b5214d453e6b95636f308908adf5538eb
pfioole <paul.fioole@telenet.Be>
2020-02-09 19:55:19 +0100

    single page first version

--- static/js/cloudds.js
+++ static/js/cloudds.js
@@ -15,8 +15,12 @@ function Sort(sortField, sortDirection) {
 }
 
 
-function StudyQuery(SQLquery, domainList, selectedFields, SQLfieldList, sortFields, leftFile, rightFile) {
+function StudyQuery(name, description, scope, dataModel, SQLquery, domainList, selectedFields, SQLfieldList, sortFields, leftFile, rightFile) {
     var self = this;
+    self.name = ko.observable("");
+    self.description = ko.observable();
+    self.scope = ko.observable();
+    self.dataModel = ko.observable();
     self.SQLquery = ko.observable("");
     self.domainList =  ko.observableArray();
     self.selectedFields = ko.observableArray();
@@ -33,21 +37,22 @@ function SinglepageViewModel() {
     self.studypathText = ko.observable("C:/CDR/3945/56021927PCR1024/DRMdata/");
     self.datasetsList = ko.observableArray();
     self.datasetsLijst = [];
-    self.leftFile = ko.observable();
-    self.rightFile = ko.observable();
+    //self.leftFile = ko.observable();
+    //self.rightFile = ko.observable();
     self.leftFieldList = ko.observable();
     self.rightFieldList = ko.observable();
     self.leftField = ko.observable();
     self.rightField = ko.observable();
-    self.selectedFields = ko.observableArray();
-    self.joinTypes = ["INNER JOIN", "OUTER JOIN", "LEFT JOIN", "RIGHT JOIN"];
+    //self.selectedFields = ko.observableArray();
+    self.joinTypes = ["INNER JOIN", "OUTER JOIN", "LEFT JOIN"];
     self.sortDirections = ["ASC", "DESC"];
     self.whereClause = ko.observable("");
     self.SQLquery = ko.observable();
     self.links = ko.observableArray([]);
     self.sorts = ko.observableArray([]);
-    self.sq = new StudyQuery("SQL statement", [,], [], [,], [,],"" ,
-        "" );
+    self.sqé = new StudyQuery()
+    self.sq = new StudyQuery("","",  "", "","", [,],
+        [], [,], [,],"" , "" );
 //    self.sortField = ko.observable();
 //    self.sortFields = ko.observableArray([]);
 
@@ -65,6 +70,7 @@ function SinglepageViewModel() {
             self.sq.domainList(lijst);
             //sqstring = JSON.stringify(self.sq);
             //alert(sqstring);
+            $("#MasterDiv").removeClass('d-none'); //view the MasterDiv of the QueryBuilder
         }).fail(function (xhr, status, error) {
             alert("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
         });
@@ -82,7 +88,7 @@ function SinglepageViewModel() {
             self.leftFieldList(lijst);
             self.links.removeAll();
             self.sorts.removeAll();
-            self.selectedFields.removeAll();
+            self.sq.selectedFields.removeAll();
 
         }).fail(function (xhr, status, error) {
             alert("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
@@ -92,16 +98,16 @@ function SinglepageViewModel() {
     self.fetchRightFields = function() {
         $.getJSON("/fetchfields", {
             studyPath: self.studypathText(),
-            selectedDomain: self.rightFile(),
+            selectedDomain: self.sq.rightFile(),
         }).done(function (result, status, xhr) {
             lijst = result.fieldList;
             for (i=0; i < lijst.length; i++) {
-                lijst[i] = self.rightFile() + "." + lijst[i];
+                lijst[i] = self.sq.rightFile() + "." + lijst[i];
             }
             self.rightFieldList(lijst);
             self.links.removeAll();
             self.sorts.removeAll();
-            self.selectedFields.removeAll();
+            self.sq.selectedFields.removeAll();
         }).fail(function (xhr, status, error) {
             alert("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
         });
@@ -113,6 +119,10 @@ function SinglepageViewModel() {
         //alert("array: " + self.links()[0].toString());
     };
 
+    this.leftDomainField = ko.computed(function() {
+        return this.sq.leftFile() + " " + this.leftField();
+    }, this);
+
     self.addSort = function() {
         self.sorts.push(new Sort("", ""));
         //alert("array: " + self.links()[0].toString());
@@ -121,21 +131,21 @@ function SinglepageViewModel() {
 
     self.addLeftFieldSelect = function() {
         //alert("selected: " + self.rightField());
-        //insert = self.leftFile() + "." + self.leftField();
-        self.selectedFields.push(self.leftField());
+        domainField = self.sq.leftFile() + "." + self.leftField();
+        self.sq.selectedFields.push(domainField);
     }
 
     self.addRightFieldSelect = function() {
         //alert("selected: " + self.rightField());
         //insert = self.rightFile() + "." + self.rightField();
-        self.selectedFields.push(self.rightField());
+        self.sq.selectedFields.push(self.rightField());
     }
 
     self.buildSQLstring = function() {
          linksLength =  self.links().length;
         if (linksLength > 0) {
-            joinText = " " + self.links()[0].joinType() + " " + self.rightFile() + " ON (" +
-                self.links()[0].leftLink() + " = " + self.links()[0].rightLink();
+            joinText = " " + self.links()[0].joinType() + " " + self.sq.rightFile() + " ON (" +
+                self.sq.leftFile() + "." + self.links()[0].leftLink() + " = " + self.links()[0].rightLink();
             if (linksLength > 1) {
                 for (i = 1; i < linksLength; i++) {
                     joinText += ") AND (" + self.links()[i].leftLink() + " = " + self.links()[i].rightLink();
@@ -163,7 +173,7 @@ function SinglepageViewModel() {
             whereSQL = "";
         }
 
-        selection = self.selectedFields().toString();
+        selection = self.sq.selectedFields().toString();
         selection = selection.replace(/,/g, "\, ");
         //,str.replace(/blue/g, "red");
 
@@ -177,7 +187,7 @@ function SinglepageViewModel() {
 
     self.runQuery4 = function() {
         self.buildSQLstring();
-        self.sq.selectedFields = self.selectedFields();
+        //self.sq.selectedFields = self.selectedFields();
         //alert("JS sent selected fields : " + self.sq.selectedFields);
         sqstring = JSON.stringify(ko.toJS(self.sq));
         $.getJSON('/fetchQueryData', {
@@ -214,14 +224,14 @@ function SinglepageViewModel() {
 
     self.runSQLquery = function() {
         self.buildSQLstring();
-        self.sq.selectedFields = self.selectedFields();
+        //self.sq.selectedFields = self.selectedFields();
         //alert("JS sent selected fields : " + self.sq.selectedFields);
         sqstring = JSON.stringify(ko.toJS(self.sq));
         $.getJSON('/fetchSQLdata', {
             studyPath: self.studypathText(),
             sqstring: sqstring
         }, function(data, status, xhr) {
-            //$("#elements").text(data.number_elements);
+            //alert("full data: " + JSON.stringify(data));
             if (qTable !== null) {
               qTable.destroy();
               qTable = null;
@@ -232,161 +242,31 @@ function SinglepageViewModel() {
 	        $("#queryTable").html(headings);
 
             qTable = $("#queryTable").DataTable({
-              data: data.data,
-              columns: data.cols
+                columns: data.cols,
+                data: data.data,
+                columnDefs: data.coldefs
             });
-
-             headings = "<tr>";
+            /*
+            headings = "<tr>";
 	        for (i=0; i< data.cols.length; i++) {
 		        headings += "<th>" + data.cols[i].data + "</th>"
                 //headings += "<th>" + result.cols[i] + "</th>"
 	        }
 	        headings += "</tr>";
 	        $("#queryTableHead").html(headings);
-
-
+            */
+            $("#QueryBuilderDiv").addClass('d-none');
+            $("#btnEditQuery").removeClass('d-none');
+            $("#btnRunQuery").addClass('d-none');
         });
         return false;
     }
 
-/*
-    self.runInitial = function() {
-        self.buildSQLstring();
-        self.sq.selectedFields = self.selectedFields();
-        //alert("JS sent selected fields : " + self.sq.selectedFields);
-        sqstring = JSON.stringify(ko.toJS(self.sq));
-
-        var qTable = $('#queryTable').DataTable({
-            scrollCollapse: false,
-            paging: true,
-            pagingType: 'simple_numbers',
-            lengthMenu: [[10, 25, 50, 100, -1], [10, 25, 50, 100, 'All']],
-            processing: true,
-            serverSide: true,
-            ajax: {
-                url: "/fetchQueryData",
-                data: {
-                    studyPath: self.studypathText(),
-                    sqstring: sqstring,
-                    //draw: parseInt(1),
-                    length: (10),
-                    start: parseInt(1)
-                }
-            },
-            searching: false,
-            info: false,
-        });
+    self.editSQLquery = function() {
+        $("#QueryBuilderDiv").removeClass('d-none');
+        $("#btnEditQuery").addClass('d-none');
+        $("#btnRunQuery").removeClass('d-none');
     }
-
-    self.runQuery3 = function () {
-        self.buildSQLstring();
-        sqstring = JSON.stringify(ko.toJS(self.sq));
-
-        $.getJSON( '/fetchQueryData', {
-                studyPath: self.studypathText(),
-                sqstring: sqstring
-            }, function ( json ) {
-            //table.destroy();
-            //$('#queryTable').empty(); // empty in case the columns change
-            headings = "<thead><tr>";
-	        for (i=0; i< data.cols.length; i++) {
-		        headings += "<th>" + data.cols[i].data + "</th>";
-                //headings += "<th>" + result.cols[i] + "</th>"
-	        }
-	        headings += "</tr></thead>";
-            var qTable = $('#queryTable').DataTable( {
-                columns: json.cols,
-                data:    json.data
-            } );
-        } );
-    };
-
-    self.runQuery2 = function() {
-        self.buildSQLstring();
-        self.sq.selectedFields = self.selectedFields();
-        //alert("JS sent selected fields : " + self.sq.selectedFields);
-        sqstring = JSON.stringify(ko.toJS(self.sq));
-        $.getJSON("/fetchQueryData", {
-            studyPath: self.studypathText(),
-            sqstring: sqstring
-        }).done(function (result, status, xhr) {
-            selectlijst = JSON.stringify(result);
-            alert("volledig lijst non stringified = " + selectlijst);
-            data.content = result.data;
-            // data.content = lijst.data;
-            data.cols = result.cols;
-
-            if (firstround == true) {
-                alert('firstround is true');
-            } else {
-                alert("in the loop");
-                qTable.destroy();
-                $('#queryTable').empty(); // empty in case the columns change
-            }
-
-            //qTable.destroy();
-            //$('#queryTable').empty(); // empty in case the columns change
-
-            headings = "<thead><tr>";
-	        for (i=0; i< data.cols.length; i++) {
-		        headings += "<th>" + data.cols[i].data + "</th>";
-                //headings += "<th>" + result.cols[i] + "</th>"
-	        }
-	        headings += "</tr></thead>";
-
-
-	        //data.content = result.data
-	        alert("headings: " + headings);
-
-            //qTable.destroy();
-            $('#queryTable').empty();
- 	        $("#queryTable").html(headings);
- 	        if (firstround == true) {
- 	            var qTable = $('#queryTable').DataTable({ data:data.content, columns: data.cols} );
-            } else {
- 	            qTable = $('#queryTable').DataTable({ data:data.content, columns: data.cols} );
-            }
-            firstround = false;
-
-            // $('#queryTable').DataTable({destroy: true ,data: data.content, columns: data.cols} );
-        }).fail(function (xhr, status, error) {
-            alert("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
-        });
-
-    }
-
-    self.runQuery = function() {
-        self.buildSQLstring();
-        self.sq.selectedFields = self.selectedFields();
-        //alert("JS sent selected fields : " + self.sq.selectedFields);
-        sqstring = JSON.stringify(ko.toJS(self.sq));
-        $.getJSON("/fetchQueryData", {
-            studyPath: self.studypathText(),
-            sqstring: sqstring
-        }).done(function (result, status, xhr) {
-            selectlijst = JSON.stringify(result);
-            alert("volledig lijst non stringified = " + selectlijst);
-            data.content = result.data;
-            // data.content = lijst.data;
-            data.cols = result.cols;
-
-            headings = "";
-	        for (i=0; i< data.cols.length; i++) {
-		        headings += "<th>" + data.cols[i].data + "</th>"
-                //headings += "<th>" + result.cols[i] + "</th>"
-	        }
-	        //data.content = result.data
-	        //alert("headings: " + headings);
-	        $("#queryTableHead").html(headings);
-            $('#queryTable').DataTable({destroy: true, data:data.content, columns: data.cols} );
-
-            // $('#queryTable').DataTable({destroy: true ,data: data.content, columns: data.cols} );
-        }).fail(function (xhr, status, error) {
-            alert("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
-        });
-
-    }
-*/
 
     self.saveQueryModal = function() {
         self.buildSQLstring();
@@ -394,60 +274,98 @@ function SinglepageViewModel() {
     }
 
     self.saveQuery = function() {
+        sqstring = JSON.stringify(ko.toJS(self.sq));
+        $.getJSON("/saveQuery", {
+            sqstring: sqstring,
+        }).done(function (result, status, xhr) {
+            alert("saved data: " + JSON.stringify(result));
+            $('#saveModal').modal('toggle');
+        }).fail(function (xhr, status, error) {
+            alert("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
+        });
+    }
 
+    self.loadQueries = function() {
+        sqstring = JSON.stringify(ko.toJS(self.sq));
+        $.getJSON("/fetchQueries", {
+            sqstring: sqstring,
+        }).done(function (result, status, xhr) {
+            //alert("full data: " + JSON.stringify(result));
+            list = result["queryList"]
+            $("#loadList").html("");
+
+            for (i=0; i<list.length; i++ ) {
+                element = '<li class="list-group-item" data-bind="click: function(){viewQuery(' + list[i] + ')}">' + list[i] + '</li>'
+                $("#loadList").append(element);
+            }
+
+
+
+
+
+            /*
+            loadTableHTML = '<table><thead><th>Query</th><th>click to load</th></thead><tbody>'
+            for (i=0; i<list.length; i++ ) {
+                loadTableHTML += "<tr><td>" + list[i] + '</td><td><button type="button" ' +
+                    ' class="btn btn-outline-primary btn-xs" ' +
+                    ' data-bind="click: function(){ root.viewQuery(';
+                loadTableHTML +=    "'" + list[i] +  "'" ;
+                loadTableHTML += '); }">Load</button></td></tr>';
+            }
+            loadTableHTML += '</tbody></thead>';
+            loadTableHTML += '<button type="button" class="btn btn-outline-primary btn-xs"  data-bind="click: test">test</button>';
+
+            alert(loadTableHTML);
+            $("#loadPane").html(loadTableHTML);
+            */
+            //$('#saveModal').modal('toggle');
+        }).fail(function (xhr, status, error) {
+            alert("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
+        });
+    }
+
+    self.test = function(){
+        alert("TEST OK!");
+    }
+    self.viewQuery = function(queryFile) {
+        alert("queryfile : " + queryFile);
     }
 
     self.removeLink = function(link) {self.links.remove(link)}
 
     self.removeSort = function(sort) {self.sorts.remove(sort)}
 
-    self.removeSelectField = function() { { self.selectedFields.pop();} }
+    self.removeSelectField = function() { { self.sq.selectedFields.pop();} }
 
-    self.removeAllLinks = function() {
-
-    }
+    self.removeAllLinks = function() {    }
 
 
 }
 
-function print() {}
+
 
 ko.applyBindings(new SinglepageViewModel());
-var firstround = true;
+//var firstround = true;
+
+viewQuery = function(queryFile) {
+        alert("queryfile : " + queryFile);
+    }
 
 
-/*
-$('#submit').on( 'click', function () {
-    $.getJSON( '/fetchQueryData', {
-            studyPath: "C:/CDR/3945/56021927PCR1024/DRMdata/",
-            sqstring: sqstring
-        }, function ( json ) {
-        //table.destroy();
-        //$('#queryTable').empty(); // empty in case the columns change
-
-        var qTable = $('#queryTable').DataTable( {
-            columns: json.cols,
-            data:    json.data
-        } );
-    } );
-} );
-*/
 
 let data = { "content" :
-	[
-		{
-			"SITEID":		"Tiger",
-			"STUDYID":	"Architect",
-            "SUBJID":     "NaN"
-		},
-		{
-			"SUBJID":       "123",
-		    "SITEID":		"Garret",
-			"STUDYID":	"Director"
-		}
-	],
-
-
+                [
+                    {
+                        "SITEID":		"Tiger",
+                        "STUDYID":	"Architect",
+                        "SUBJID":     "NaN"
+                    },
+                    {
+                        "SUBJID":       "123",
+                        "SITEID":		"Garret",
+                        "STUDYID":	"Director"
+                    }
+                ],
 			"cols" :
 				[
 				    {data: "SITEID"},
@@ -459,7 +377,5 @@ let data = { "content" :
 
 $(document).ready( function () {
 
-    //var qTable = $('#queryTable').DataTable();
-    //var qTable = $('#queryTable').DataTable();
 } );
 
